4df350953d7e781484cd445c625208be1c6e08b3
Porkinho <169182285+PorkoOnRails@users.noreply.github.com>
2026-02-25 15:08:38 -0300

    Calculator3000
    Starting to make in JavaScript with html and css

---
+++ Calculator3000/script.js
@@ -0,0 +1,47 @@
+function inputAdd(num1, num2) {
+
+    var num1 = prompt("Enter the fist number");
+    var num1Checked = Number(num1);
+
+    var num2 = prompt("Enter the second number");
+    var num2Checked = Number(num2);
+
+    alert("The some of the numbers is " + (num1Checked + num2Checked));
+
+}
+
+function inputSub(num1, num2) {
+
+    var num1 = prompt("Enter the fist number");
+    var num1Checked = Number(num1);
+
+    var num2 = prompt("Enter the second number");
+    var num2Checked = Number(num2);
+
+    alert("The some of the numbers is " + (num1Checked - num2Checked));
+
+}
+
+function inputDiv(num1, num2) {
+
+    var num1 = prompt("Enter the fist number");
+    var num1Checked = Number(num1);
+
+    var num2 = prompt("Enter the second number");
+    var num2Checked = Number(num2);
+
+    alert("The some of the numbers is " + (num1Checked / num2Checked));
+
+}
+
+function inputMult(num1, num2) {
+
+    var num1 = prompt("Enter the fist number");
+    var num1Checked = Number(num1);
+
+    var num2 = prompt("Enter the second number");
+    var num2Checked = Number(num2);
+
+    alert("The some of the numbers is " + (num1Checked * num2Checked));
+
+}
